93167f4aec9c8b119d16c80be2b1c64f3b8bac01
Fardin96 <fardinshuvro96@gmail.com>
2025-05-18 19:59:49 +0600

    redis client logs

--- app/utils/redisClient.ts
+++ app/utils/redisClient.ts
@@ -3,6 +3,14 @@ import { createClient } from 'redis';
 // const redis = await createClient({ url: process.env.REDIS_URL }).connect();
 const redisClient = await createClient({
   url: process.env.REDIS_URL,
-}).connect();
+})
+  .connect()
+  .then(() => {
+    console.log('Redis connected successfully!');
+    return redisClient;
+  })
+  .catch((error) => {
+    console.log('Error @ redisClient: ', error);
+  });
 
 export default redisClient;

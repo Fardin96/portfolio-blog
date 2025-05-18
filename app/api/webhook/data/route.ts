a91932727c3908f40647b063701f0ef4c504929b
Fardin96 <fardinshuvro96@gmail.com>
2025-05-18 18:37:07 +0600

    redis get update

--- app/api/webhook/data/route.ts
+++ app/api/webhook/data/route.ts
@@ -9,11 +9,22 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
   try {
     const webhookData = await redisClient.get('webhookData');
 
+    let webhooks = [];
+    if (webhookData) {
+      webhooks = await JSON.parse(webhookData as string);
+
+      // convert to array if not already
+      if (!Array.isArray(webhooks)) {
+        webhooks = [webhooks];
+      }
+    }
+
     return NextResponse.json({
-      webhooks: webhookData || [],
+      webhooks: webhooks,
     } as WebhookDataResponse);
   } catch (error) {
     console.log('Error @ webhook-GET: ', error);
+
     return NextResponse.json(
       { error: 'Webhook GET error!', webhooks: [] } as WebhookDataResponse,
       { status: 400 }
--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ async function fetchWebhookData(
       throw new Error('Failed to fetch webhook data');
     }
 
-    const data = await response.json();
+    const data = (await response.json()) as WebhookData[];
     console.log('+--------------------main-page------------------+');
     console.log('data: ', data);
 

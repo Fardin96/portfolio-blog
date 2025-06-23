ea03bc7b1cd3cc000d516c935aff7f343d8be56a
Fardin96 <fardinshuvro96@gmail.com>
2025-06-23 16:17:03 +0600

    bloglisting - cleaned up

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,22 +1,12 @@
 import Link from 'next/link';
-// import { useEffect, useState } from 'react';
-import { AllPosts, BlogPost } from '../../public/types/types';
-import { getAllPosts } from '../../utils/sanityServices';
+import { BlogPost } from '../../public/types/types';
 import { formatDate } from '../../utils/utils';
-import { fetchWebhookData } from '../../utils/webhookServices';
-import { WebhookData } from '../../public/types/webhookTypes';
-import { getGithubPosts, getRepositoryData } from '../../utils/githubServices';
+import { getGithubPosts } from '../../utils/githubServices';
 
 async function fetchGithubBlogs(): Promise<BlogPost[]> {
   try {
-    // const response = await fetch(
-    //   // '/api/github/repository?path=http-response-fundamentals/http-response-fundamentals.md'
-    //   '/api/github/repository'
-    // );
     const data = await getGithubPosts('');
 
-    // const result = await data.json();
-    console.log('GitHub repository data:', data);
     return data;
   } catch (error) {
     console.error('Error fetching repository data:', error);
@@ -24,65 +14,7 @@ async function fetchGithubBlogs(): Promise<BlogPost[]> {
 }
 
 export default async function Blogs(): Promise<React.ReactElement> {
-  // const [data, setData] = useState<AllPosts[]>([]);
-  // const [error, setError] = useState<string>('');
-  // const [loading, setLoading] = useState<boolean>(true);
-  // const [webhookData, setWebhookData] = useState<WebhookData | null>(null);
-
   const data: BlogPost[] = await fetchGithubBlogs();
-
-  // const fetchSanityData = async (): Promise<void> => {
-  //   setError('');
-
-  //   try {
-  //     const result = await getAllPosts('blog');
-  //     // setData(result);
-  //   } catch (err) {
-  //     console.error('Failed to fetch posts:', err);
-
-  //     const errMsg = err instanceof Error ? err.message : 'Unknown Error';
-  //     setError(`Failed to fetch data: ${errMsg}`);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
-
-  // let res;
-  // useEffect(() => {
-  //   (async () => {
-  //     // await fetchWebhookData(setWebhookData);
-  //     await fetchGithubBlogs();
-  //   })();
-  // }, []);
-
-  // setData(res);
-  // console.log('posts: ', JSON.stringify(res, null, 2));
-
-  // useEffect(() => {
-  //   fetchSanityData();
-  // }, []);
-
-  // // error view
-  // if (error.length > 0 && data.length === 0) {
-  //   return (
-  //     <div>
-  //       <h1 className='text-3xl font-bold mb-6'>My Blog</h1>
-
-  //       <h3>{error}</h3>
-  //     </div>
-  //   );
-  // }
-
-  // // loading view
-  // if (loading) {
-  //   return (
-  //     <div>
-  //       <h1 className='text-3xl font-bold mb-6'>My Blog</h1>
-
-  //       <h3>loading...</h3>
-  //     </div>
-  //   );
-  // }
 
   // empty view
   if (data.length === 0) {
@@ -90,11 +22,12 @@ export default async function Blogs(): Promise<React.ReactElement> {
       <div>
         <h1 className='text-3xl font-bold mb-6'>My Blog</h1>
 
-        <h3>no projects yet :(</h3>
+        <h3>no blogs yet :(</h3>
       </div>
     );
   }
 
+  // main view
   return (
     <div>
       <h1 className='text-3xl font-bold mb-6'>My Blogs</h1>

f8a2fb7c97b853d8e449ce487934ca8809a49af9
Fardin96 <fardinshuvro96@gmail.com>
2025-06-30 14:58:06 +0600

    md to html - converter & packages

---
+++ utils/mdToHtmlService.ts
@@ -0,0 +1,20 @@
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeStringify from 'rehype-stringify';
+import remarkGfm from 'remark-gfm';
+import rehypeSlug from 'rehype-slug';
+import rehypeHighlight from 'rehype-highlight';
+
+export async function mdToHtml(mdContent: string): Promise<string> {
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeSlug)
+    .use(rehypeHighlight)
+    .use(rehypeStringify, { allowDangerousHtml: true })
+    .process(mdContent);
+
+  return result.toString();
+}
